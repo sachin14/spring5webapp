8a54dfce639d16692d40de35bf0b4659333b9a89
sachin jamthe <>
2020-03-16 22:04:27 -0500

    new file

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,4 +1,42 @@
 package guru.springframework.spring5webapp.domain;
 
 public class Author {
+
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Author(){
+
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
